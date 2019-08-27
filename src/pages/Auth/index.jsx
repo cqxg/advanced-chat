7127bf2b221057b1f60c37e9270196042862ce20
cqxg <cqxg@yandex.ru>
2019-08-27 18:33:08 +0300

    feat: add input preparation

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Block, Button } from '../../components';
+import { Input } from 'antd';
+import { Button, Block } from 'components';
 
-const Auth = () => {
-    return (
-        <div>
+import './Auth.scss';
+
+const Auth = () => (
+    <section className='auth'>
+        <div className="auth__content">
+            <div className="auth__top">
+                <h2>Войти в аккаунт</h2>
+                <p>Пожалуйста, войдите в свой аккаунт</p>
+            </div>
             <Block >
+                <Input placeholder='qwe' />
                 <Button className='button__large' type='primary' size='large'>
                     This is button
-                    </Button>
+            </Button>
             </Block>
         </div>
-    )
-}
+    </section>
+)
 
 export default Auth;
