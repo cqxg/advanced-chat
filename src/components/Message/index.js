e4e0ceff80f3cff77703917921533c3703bbf083
cqxg <cqxg@yandex.ru>
2019-09-01 22:25:31 +0300

    feat: add dynamic time

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames'
+import { distanceInWordsToNow } from 'date-fns/distance_in_words_to_now';
 
 import './Message.scss'
 
@@ -13,7 +13,7 @@ const Message = ({ avatar, user, text, date }) => (
             <div className='message__bubble'>
                 <p className='message__text'>{text}</p>
             </div>
-            <span className='message__date'>21:31 Jun 2019</span>
+            <span className='message__date'>{distanceInWordsToNow(date)}</span>
         </div>
     </div>
 );
